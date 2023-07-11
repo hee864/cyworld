9abb0a2e9a9b526951a1a58679d6cd3f9688870e
hee864 <wkrldowk1@naver.com>
2023-07-11 09:50:07 +0900

    second commit

--- class/final/final.js
+++ class/final/final.js
@@ -1,31 +1,44 @@
+
 const changeFocus1=()=>{
     let phone1=document.getElementById("p1").value
+    document.getElementById("p1").addEventListener("input", function(e) {
+        if (e.target.value.length > 3) {
+            e.target.value = e.target.value.slice(0, 3);
+        }
+    });
     if(phone1.length===3){
         document.getElementById("p2").focus()
+        
     }
+    
 }
 const changeFocus2=()=>{
+
     let phone2=document.getElementById("p2").value
+    document.getElementById("p2").addEventListener("input", function(e) {
+        if (e.target.value.length > 4) {
+            e.target.value = e.target.value.slice(0, 4);
+        }
+    });
     if(phone2.length===4){
         document.getElementById("p3").focus()
     }
 }
 const changeFocus3=()=>{
     let phone3=document.getElementById("p3").value
+    document.getElementById("p3").addEventListener("input", function(e) {
+        if (e.target.value.length > 4) {
+            e.target.value = e.target.value.slice(0, 4);
+        }
+    });
     if(phone3.length===4){
         document.getElementById("sendMessage").focus()
         document.getElementById("sendMessage").disabled=false
     }
 }//전화번호 마지막 4자리 입력시 인증번호 보내기 활성화
-const init=()=>{
-    document.getElementById("sendMessage").disabled=true
-    document.getElementById("complete").disabled=true
-    document.getElementById("target").innerHTML="000000"
-    document.getElementById("timer").innerHTML="3:00"
-    document.getElementById("sendMessage").setAttribute("style","background-color:none;")
-    document.getElementById("complete").setAttribute("style","background-color:none;")
-}//초기화 함수
 
+
+var timer;
 let isStarted=false
 
 let auth=()=>{
@@ -38,11 +51,19 @@ let auth=()=>{
         document.getElementById("complete").disabled=false;
         const randomNum=String(Math.floor(Math.random()*1000000)).padStart(6,"0")
        document.getElementById("target").innerText=randomNum
-        let time=10
-        let timer
-        timer=setInterval(function(){
+       document.getElementById("sendMessage").disabled=true
+        
+       let time=180
+        
+       timer=setInterval(function(){
             if(time>=0)
             {
+                if(document.getElementById("complete").clicked==true)
+                {
+                    checkComplete()
+
+                }
+                
                 let min=Math.floor(time/60)
                 let sec=String(time%60).padStart(2,"0")
                 document.getElementById("timer").innerText=min+":"+sec
@@ -51,21 +72,89 @@ let auth=()=>{
             }
             else{
                 document.getElementById("complete").disabled=true
+                document.getElementById("target").innerText="000000"
+                document.getElementById("timer").innerText="3:00"
                 isStarted=false;//타이머 종료와 함께 if명령문 통과 가능
-                console.log("타이머 작동중")
+                
                 clearInterval(timer)//setInterval 함수를 해제시킴
             }
         },1000)
     }else{
         //타이머 작동중
     }
+    
 }
 const checkComplete=()=>{//문자인증이 완료되었을때
-    init();
-    document.getElementById("complete").innerHTML="인증완료"
-    document.getElementById("SignUp").disabled=false
-    document.getElementById("signUp").setAttribute("style","background-color:blue;")
+    
+    alert("인증이 확인되었습니다.");
+    document.getElementById("complete").innerText="인증완료"
+    document.getElementById("signUp").disabled=false
+
+    document.getElementById("sendMessage").disabled=true
+    document.getElementById("complete").disabled=true
+
+    document.getElementById("target").innerText="000000"
+    document.getElementById("timer").innerText="3:00"
+    clearInterval (timer)
+
+    isStarted=false;
+   
 }
-const signupCheck=()=>{
-    //회원가입 사항 체크
+const checkValue=()=>{
+    var email=document.getElementById("email").value;
+    
+
+    var name=document.getElementById("name").value;
+    
+    
+    var password=document.getElementById("password").value;
+    var passwordCheck=document.getElementById("passwordCheck").value;
+    
+
+    var regionSelect=document.getElementById("area");
+    var selectOption=regionSelect.options[regionSelect.selectedIndex];
+
+    
+    var gender_woman=document.getElementById("gender_woman");
+    var gender_man=document.getElementById("gender_man"); 
+    
+    var value=true
+    
+    if(!email  ){
+        document.getElementById("emailError").style="block";
+        value=false
+       
+    } else if(!email.includes("@")){
+        document.getElementById("emailError").style="block";
+    }
+    if(!name){
+        document.getElementById("nameError").style="block";
+        value=false
+    } 
+   
+    if(!password){
+        document.getElementById("passwordError").style="block";
+        value=false
+    }
+   
+    if((!passwordCheck)||(password!==passwordCheck) ){
+        document.getElementById("passwordCheckError").style="block";
+        value=false
+    }
+    if(selectOption.value==="지역을 선택하세요"){
+        
+        document.getElementById("areaError").style="block";
+        value=false
+    }
+    if(!gender_man.checked &&!gender_woman.checked){
+        document.getElementById("genderError").style="block";
+        value=false
+    }
+
+    if(value===true){
+        alert("회원가입축하")
+    }
+    
+    
+   
 }
